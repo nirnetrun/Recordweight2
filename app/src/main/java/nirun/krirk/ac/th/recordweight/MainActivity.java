81c8376d14e494b95ee9ec151316280d3b261295
nirun <ngamkerd@gmail.com>
2018-02-06 12:06:10 +0700

    feb 06 commit and put

--- app/src/main/java/nirun/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/nirun/krirk/ac/th/recordweight/MainActivity.java
@@ -1,9 +1,11 @@
 package nirun.krirk.ac.th.recordweight;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import nirun.krirk.ac.th.recordweight.utility.MyManager;
+import nirun.krirk.ac.th.recordweight.utility.MyOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 //        Test and user
+//        testAndUser();
+
+
+//        Double all userTABLE;
+//        deleteAlluserTABLE();
+
+
+    }   //main method
+
+    private void delete() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.databaseName,
+                MODE_PRIVATE, null);
+
+        sqLiteDatabase.delete("userTABLE", null, null);
+    }
+
+    private void testAndUser() {
         myManager.addValueToUserTable("nickName",
                 "SrartApp",
                 "Weight",
@@ -28,6 +47,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-}
+}//mian class
